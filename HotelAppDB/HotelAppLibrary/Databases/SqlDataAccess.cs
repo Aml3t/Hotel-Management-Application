4d7b7b655f7e5827977ed401a5608817430d086d
Aml3t <fischerparsos@gmail.com>
2023-06-30 16:02:19 +0300

    Creating the data logic for the UI's

--- HotelAppDB/HotelAppLibrary/Databases/SqlDataAccess.cs
+++ HotelAppDB/HotelAppLibrary/Databases/SqlDataAccess.cs
@@ -18,19 +18,35 @@ namespace HotelAppLibrary.Databases
         {
             _config = config;
         }
+
         public List<T> LoadData<T, U>(string sqlStatement,
                                       U parameters,
                                       string connectionStringName,
-                                      dynamic options)
+                                      dynamic options = null)
         {
             string connectionString = _config.GetConnectionString(connectionStringName);
+            CommandType commandType = CommandType.Text; //Default value of commandType
+
+            if (options.IsStoredProcedure != null && options.IsStoredProcedure == true) //Check whether default or Stored Procedure
+            {
+                commandType = CommandType.StoredProcedure;
+            }
+
 
             using (IDbConnection connection = new SqlConnection())
             {
 
-                List<T> rows = connection.Query<T>(sqlStatement, parameters).ToList();
+                List<T> rows = connection.Query<T>(sqlStatement, parameters, commandType: commandType).ToList();
                 return rows;
             }
+        }
+
+        public void SaveData<T>(string sqlStatement,
+                                T parameters,
+                                string connectionStringName,
+                                dynamic options = null)
+        {
+
         }
     }
 }
