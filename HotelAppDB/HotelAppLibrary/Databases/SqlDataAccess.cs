afc9685bbc01811d0612dc12a90eb856c933c8f5
Aml3t <fischerparsos@gmail.com>
2023-06-30 14:42:38 +0300

    Creating the data logic for the UI's

---
+++ HotelAppDB/HotelAppLibrary/Databases/SqlDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAppLibrary.Databases
+{
+    public class SqlDataAccess
+    {
+        private readonly IConfiguration config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            this.config = config;
+        }
+        public List<T> LoadData<T, U>(string sqlStatement, U parameters)
+        {
+            using (IDbConnection connection = new SqlConnection())
+            {
+                var p = new DynamicParameters();
+
+                p.Add("MyParameter", parameter);
+                connection.Execute("spWriteToSql", p, commandType: CommandType.StoredProcedure);
+
+            }
+        }
+    }
+}
