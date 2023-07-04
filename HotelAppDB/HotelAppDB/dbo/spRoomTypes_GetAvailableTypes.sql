6a4d711d8292c0d2eea428a5b50df70b6af2fede
Aml3t <fischerparsos@gmail.com>
2023-07-04 15:14:33 +0300

    Adding the available room functionallity

--- HotelAppDB/HotelAppDB/dbo/spRoomTypes_GetAvailableTypes.sql
+++ HotelAppDB/HotelAppDB/dbo/spRoomTypes_GetAvailableTypes.sql
@@ -5,5 +5,16 @@ AS
 begin
 	set nocount on;
 
+	select t.Id, t.Title, t.Description ,t.Price
+	from dbo.Rooms r
+	inner join dbo.RoomTypes t on t.id = r.RoomTypeId
+	where r.id not in(
+	select b.RoomId
+	from dbo.Bookings b
+	where (@startDate < b.StartDate and @endDate > b.EndDate)
+	or (b.StartDate <= @endDate and @endDate < b.EndDate)
+	or (b.StartDate <= @startDate and @startDate < b.EndDate)
+	)
 
+group by t.id, t.Title, t.Description ,t.Price
 end
