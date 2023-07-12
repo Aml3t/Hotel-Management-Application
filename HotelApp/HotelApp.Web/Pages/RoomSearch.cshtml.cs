ed8f0bdfd1ef53e209d47e63a32e100ec91e78ad
Aml3t <fischerparsos@gmail.com>
2023-07-12 13:19:01 +0300

    Web: Searching a room

--- HotelApp/HotelApp.Web/Pages/RoomSearch.cshtml.cs
+++ HotelApp/HotelApp.Web/Pages/RoomSearch.cshtml.cs
@@ -1,6 +1,8 @@
+using HotelAppLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace HotelApp.Web.Pages
@@ -8,20 +10,30 @@ namespace HotelApp.Web.Pages
     public class RoomSearchModel : PageModel
     {
         [DataType(DataType.Date)]
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public DateTime StartDate { get; set; } = DateTime.Now;
 
         [DataType(DataType.Date)]
-        [BindProperty]
+        [BindProperty(SupportsGet = true)]
         public DateTime EndDate { get; set; } = DateTime.Now.AddDays(7);
+
+        [BindProperty(SupportsGet = true)]
+        public bool SearchEnabled { get; set; } = false;
+
+        public List<RoomTypeModel> AvailableRoomTypes { get; set; }
 
         public void OnGet()
         {
+            if (SearchEnabled == true)
+            {
 
+            }
         }
 
         public IActionResult OnPost()
         {
+             
+            
             return Page();
         }
     }
