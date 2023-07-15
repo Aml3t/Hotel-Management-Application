e034a17eb99ac9ecfe84d515177d161b5ea8c113
Aml3t <fischerparsos@gmail.com>
2023-07-15 13:40:38 +0300

    Setting up the WPF form

--- HotelApp/HotelApp.Desktop/App.xaml.cs
+++ HotelApp/HotelApp.Desktop/App.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +16,19 @@ namespace HotelApp.Desktop
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var services = new ServiceCollection();
+
+            //Transient because we want multiple instances. We could put Singleton if we wanted only one instance.
+            services.AddTransient<MainWindow>();
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+        }
     }
 }
