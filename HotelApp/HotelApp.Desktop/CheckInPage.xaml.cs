6875d7089c085a1d75d282fc23a1a0cb5e197efe
Aml3t <fischerparsos@gmail.com>
2023-07-17 14:44:41 +0300

    Adding the search functionality

---
+++ HotelApp/HotelApp.Desktop/CheckInPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HotelApp.Desktop
+{
+    /// <summary>
+    /// Interaction logic for CheckInPage.xaml
+    /// </summary>
+    public partial class CheckInPage : Window
+    {
+        public CheckInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void checkInId_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
