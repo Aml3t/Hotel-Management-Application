a5dd0d5c701eda244152a0854687c902379b4429
Aml3t <fischerparsos@gmail.com>
2023-08-03 15:05:52 +0300

    Adding ability to select different database

--- HotelApp/HotelAppLibrary/Data/SqliteData.cs
+++ HotelApp/HotelAppLibrary/Data/SqliteData.cs
@@ -1,12 +1,38 @@
-﻿using HotelAppLibrary.Models;
+﻿using HotelAppLibrary.Databases;
+using HotelAppLibrary.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Data;
 using System.Text;
 
 namespace HotelAppLibrary.Data
 {
     public class SqliteData : IDatabaseData
     {
+        private readonly ISqliteDataAccess _db;
+
+        private const string connectionString = "SqlDb";
+
+        public SqliteData(ISqliteDataAccess db)
+        {
+            _db = db;
+        }
+
+
+        public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
+        {
+
+            using (IDbConnection dbConnection = new SQLiteConnection(connectionString))
+            {
+                dbConnection.Open();
+
+                string query = "SELECT * FROM RoomTypes WHERE StartDate >= @startDate AND EndDate <= @endDate";
+                var parameters = new { startDate, endDate };
+
+                return dbConnection.Query<RoomTypeModel>(query, parameters).AsList();
+            }
+        }
         public void BookGuest(string FirstName, string LastName, DateTime startDate, DateTime endDate, int roomTypeId)
         {
             throw new NotImplementedException();
@@ -17,10 +43,7 @@ namespace HotelAppLibrary.Data
             throw new NotImplementedException();
         }
 
-        public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
-        {
-            throw new NotImplementedException();
-        }
+
 
         public RoomTypeModel GetRoomTypeById(int id)
         {
