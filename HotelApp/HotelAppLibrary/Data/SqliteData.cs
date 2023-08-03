a9f3571bf36391c016150a70bc4e587a4a7d0cc0
Aml3t <fischerparsos@gmail.com>
2023-08-03 15:35:16 +0300

    Adding ability to select different database

--- HotelApp/HotelAppLibrary/Data/SqliteData.cs
+++ HotelApp/HotelAppLibrary/Data/SqliteData.cs
@@ -11,24 +11,33 @@ namespace HotelAppLibrary.Data
 {
     public class SqliteData : IDatabaseData
     {
-        private readonly ISqliteDataAccess _db;
+        private const string connectionStringName = "SQLiteDb";
 
-        private const string connectionString = "SqlDb";
+        private readonly ISqliteDataAccess _db;
 
         public SqliteData(ISqliteDataAccess db)
         {
             _db = db;
         }
-
-
         public List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate)
         {
+            string sql = @"	select r.*
+	                    from dbo.Rooms r
+	                    inner join dbo.RoomTypes t on t.Id = r.RoomTypeId
+	                    where r.RoomTypeId = @roomTypeId
+	                    and	r.Id not in(
+	                    select b.RoomId
+	                    from dbo.Bookings b
+	                    where (@startDate < b.StartDate and @endDate > b.EndDate)
+	                    or (b.StartDate <= @endDate and @endDate < b.EndDate)
+	                    or (b.StartDate <= @startDate and @startDate < b.EndDate)
+	                    );";
 
-            using (IDbConnection dbConnection = new SQLiteConnection(connectionString))
+            using (IDbConnection dbConnection = new SQLiteConnection(connectionStringName))
             {
                 dbConnection.Open();
 
-                string query = "SELECT * FROM RoomTypes WHERE StartDate >= @startDate AND EndDate <= @endDate";
+                string query = sql;
                 var parameters = new { startDate, endDate };
 
                 return dbConnection.Query<RoomTypeModel>(query, parameters).AsList();
