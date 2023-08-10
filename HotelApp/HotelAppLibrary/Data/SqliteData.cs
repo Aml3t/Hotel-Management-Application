02896c2bee95d14c610d13f25d5d400633b960fb
Aml3t <fischerparsos@gmail.com>
2023-08-10 14:52:48 +0300

    Adding ability to book a person into SQLite database

--- HotelApp/HotelAppLibrary/Data/SqliteData.cs
+++ HotelApp/HotelAppLibrary/Data/SqliteData.cs
@@ -129,10 +129,18 @@ namespace HotelAppLibrary.Data
 
         public List<BookingFullModel> SearchBookings(string lastName)
         {
-            return _db.LoadData<BookingFullModel, dynamic>("dbo.spBookings_Search",
+            string sql = @"	select [b].[Id], [b].[RoomId], [b].[GuestId], [b].[StartDate], [b].[EndDate],
+		                    [b].[CheckedIn], [b].[TotalCost],[g].[FirstName], [g].[LastName],
+		                    [r].[RoomNumber], r.[RoomTypeId],[rt].[Title], [rt].[Description], [rt].[Price]
+	                        from Bookings b
+	                        inner join Guests g on b.GuestId = g.Id
+	                        inner join Rooms r on b.RoomId = r.Id
+	                        inner join RoomTypes rt on r.RoomTypeId = rt.Id
+	                        where b.StartDate = @startDate and g.LastName = @lastName;";
+
+            return _db.LoadData<BookingFullModel, dynamic>(sql,
                                                 new { lastName, startDate = DateTime.Now.Date },
-                                                connectionStringName,
-                                                true);
+                                                connectionStringName);
         }
     }
 }
